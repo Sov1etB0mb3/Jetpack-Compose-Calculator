45047cc93b003ab7de5b971316da984cf3f4bc18
Alan <nguyentrungkhanh804@gmail.com>
2026-05-25 22:07:25 +0700

    add square root, factorial, square, trigonometry and pi

--- app/src/main/java/com/teamb/calculator/action/CalculatorAction.kt
+++ app/src/main/java/com/teamb/calculator/action/CalculatorAction.kt
@@ -7,6 +7,13 @@ sealed class CalculatorAction {
     object Calculate : CalculatorAction()
     object OpenParen : CalculatorAction()
     object CloseParen : CalculatorAction()
+    object Factorial : CalculatorAction()
+    object Pi : CalculatorAction()
+    object Sin : CalculatorAction()
+    object Cos : CalculatorAction()
+    object Tan : CalculatorAction()
+    object Sqrt : CalculatorAction()
+    object Square : CalculatorAction()
     data class Number(val number: Int) : CalculatorAction()
     data class Operation(val operation: CalculatorOperation) : CalculatorAction()
 }
